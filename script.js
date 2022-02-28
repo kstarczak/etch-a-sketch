d066e6d58cb447be9d60071314f4eaf3e7c8d260
Konrad Starczak <k.starczak@gmail.com>
2022-02-28 16:34:00 -0500

    Add function to create divs and add CSS styling to arrange in grid

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 const container = document.querySelector('.container');
+const messageBox = document.querySelector('.messageBox');
+let width = 101;
+let position = [(width-1)/2,(width-1)/2]
+console.log(position)
+
+
+function createGrid(width) {
+    for (let y = 0; y < (width); y++) {
+        for (let x = 0; x < width; x++) {
+        container.appendChild(document.createElement('div'));
+        container.lastChild.classList.add('gridBox');
+        const gridBoxWidth= 800 / width;
+        container.lastChild.setAttribute(`style`, `width:${gridBoxWidth}px;height:auto`)
+        }
+    }
+}
+
+createGrid(width);
+
