f88ef5cd7503276d0ab3015a874a727266b2ca38
Konrad Starczak <k.starczak@gmail.com>
2022-03-29 12:31:43 -0400

    Make screen wider edit draw function accordingly

--- etch-a-sketch-2/script.js
+++ etch-a-sketch-2/script.js
@@ -1,45 +1,75 @@
-const container = document.querySelector('.container');
+const screen = document.querySelector('.screen');
 const messageBox = document.querySelector('.messageBox');
-let width = 101;
-let position = [(width-1)/2,(width-1)/2]
-console.log(position)
+const screenWidth = screen.offsetWidth;
+const screenHeight = screen.offsetHeight;
+let width = screenWidth;
+let height = screenHeight;
+let position = [(width) / 2, (height) / 2];
+console.log(position);
 
 
-function createGrid(width) {
-    //const gridBox = document.createElement('div');
-    //gridBox.classList.add('gridBox');
-    for (let y = 0; y < (width); y++) {
+// run function below when user changes size. lowest width is 3 and hightest is 504. CHECK IF e.value is correct
+function sizeSelector(e = 168) {
+    width = e.value * 3;
+    height = e.value * 4;
+    position = [( width) / 2, (height) / 2];
+}
+// add eventlistener and scroll bar
+// const scrollBar = document.querySelector(.....class of the scrollbar)
+// scrollBar.addEventListener('change', sizeSelector);
+
+
+
+/* create selector to pick colors
+let color = "black";
+
+
+
+function colorSelector(e = "black") {
+    color = e.value;
+}
+ */
+
+// how to replace class if one exists or create one if it does no
+
+
+
+
+function createGrid(width,height) {
+    for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
-        container.appendChild(document.createElement('div'));
-        container.lastChild.classList.add('gridBox');
-        container.lastChild.classList.add(`x-${x}`);
-        container.lastChild.classList.add(`y-${y}`);
-        const gridBoxWidth= 800 / width;
-        container.lastChild.setAttribute(`style`, `width:${gridBoxWidth}px;height:auto`)
+        screen.appendChild(document.createElement('div'));
+        screen.lastChild.classList.add('gridBox');
+        screen.lastChild.classList.add(`x-${x}`);
+        screen.lastChild.classList.add(`y-${y}`);
+            const gridBoxWidth = 1;
+            const gridBoxHeight = 1;
+        screen.lastChild.setAttribute(`style`, `width:${gridBoxWidth}px;height:${gridBoxHeight}`)
         }
     }
 }
 
-createGrid(width);
-
-
+createGrid(width,height);
 
 
 function draw(e) {
     messageBox.textContent = '';
     if (e.code === 'ArrowDown') {
-        if (position[1] < width-1) {
+        if (position[1] < height-1) {
             position[1] += 1;
             const gridDark = document.querySelector(`.x-${position[0]}.y-${position[1]}`)
             gridDark.classList.add('gridBoxDark')
+            console.log(position);
         } else {
             messageBox.textContent = 'You\'ve reached the bottom edge of the screen';
+            console.log(position);
         };
     } else if (e.code === 'ArrowUp') {
         if (position[1] > 0) {
             position[1] -= 1;
             const gridDark = document.querySelector(`.x-${position[0]}.y-${position[1]}`)
             gridDark.classList.add('gridBoxDark')
+            console.log(position);
         } else {
             messageBox.textContent = 'You\'ve reached the top edge of the screen.';
         };
@@ -48,19 +78,24 @@ function draw(e) {
             position[0] += 1;
             const gridDark = document.querySelector(`.x-${position[0]}.y-${position[1]}`)
             gridDark.classList.add('gridBoxDark');
+            console.log(position);
         } else {
             messageBox.textContent = 'You\'ve reached the right edge of the screen.';
+            console.log(position);
         };
     } else if (e.code === 'ArrowLeft') {
         if (position[0] > 0) {
             position[0] -= 1;
             const gridDark = document.querySelector(`.x-${position[0]}.y-${position[1]}`)
             gridDark.classList.add('gridBoxDark')
+            console.log(position);
         } else {
             messageBox.textContent = 'You\'ve reached the left edge of the screen.';
+            console.log(position);
         };
     } else {
         messageBox.textContent = 'press one of the arrow keys to draw.';
+        console.log(position);
     }
 }
 
